026fafed6471e53a5dbba2a31de6bc1800d1db1c
Sassan Nourian <sancomdev@gmail.com>
2023-10-13 19:39:42 +0330

    Project initiated.

---
+++ TAT/Source/TAT.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var player_s = preload("res://Source/Player/Player.tscn")
+var starting_village_s = preload("res://Source/Scenes/StartingVillageS/StartingVillageS.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var starting_village = starting_village_s.instantiate()
+	add_child(starting_village)
+	var player = player_s.instantiate()
+	add_child(player)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
